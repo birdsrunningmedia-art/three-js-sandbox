eafbe6266c5b1cdbb2f632d76d64c2dd2b7a86b9
Yekini Abdulwahab <birdsrunningmedia@gmail.com>
2025-12-01 05:45:20 +0100

    first commit

---
+++ src/main.js
@@ -0,0 +1,70 @@
+import "./style.css";
+
+import * as THREE from "three";
+
+import { OrbitControls } from "three/examples/jsm/Addons.js";
+
+const scene = new THREE.Scene();
+
+// The camera takes in perspective view, aspect ration, camera length
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+// where to render the 3D stuffs
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector("#bg"),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+
+// The basics or the template have been created
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+
+// This type of material dows not require light
+// const material = new THREE.MeshBasicMaterial({
+//   color: 0xff5347,
+//   wireframe: true,
+// });
+
+// This reacts to light bouncing off of it.
+const material = new THREE.MeshStandardMaterial({
+  color: 0xff5347,
+});
+
+const torus = new THREE.Mesh(geometry, material);
+
+scene.add(torus);
+
+// The light
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(0, 0, 0);
+scene.add(pointLight);
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(ambientLight);
+
+const lightHelper = new THREE.PointLightHelper(pointLight);
+const gridHelper = new THREE.GridHelper(200, 50);
+scene.add(lightHelper, gridHelper);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.005;
+  torus.rotation.z += 0.01;
+
+  controls.update();
+
+  renderer.render(scene, camera);
+}
+
+animate();
