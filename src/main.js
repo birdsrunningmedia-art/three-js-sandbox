f33c2dfe943d4ab290a9bd2a1a25d280ba0114c9
Yekini Abdulwahab <birdsrunningmedia@gmail.com>
2025-12-01 19:51:30 +0100

    exploration from here

--- src/main.js
+++ src/main.js
@@ -70,6 +70,33 @@ function addStar() {
 
 Array(500).fill().forEach(addStar);
 
+const moonTexture = new THREE.TextureLoader().load("/images/moon.jpg");
+const normalTexture = new THREE.TextureLoader().load("/images/normal.jpg");
+
+const moon = new THREE.Mesh(
+  new THREE.SphereGeometry(3, 32, 32),
+  new THREE.MeshStandardMaterial({
+    moonTexture,
+    normalMap: normalTexture
+  })
+);
+
+moon.position.z = 30
+moon.position.setX(-10)
+
+scene.add(moon);
+
+function moveCamera() {
+const t = document.body.getBoundingClientRect().top
+
+camera.position.z = t * -0.01
+camera.position.x = t * -0.0002
+camera.position.y = t * -0.0002
+}
+
+document.body.onscroll = moveCamera
+
+
 const spaceTexture = new THREE.TextureLoader().load("/images/space.jpg");
 scene.background = spaceTexture;
 
